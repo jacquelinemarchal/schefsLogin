68b9da0391a547ad9b4e6ad3905296a5a976dea6
jacquelinemarchal <64337067+jacquelinemarchal@users.noreply.github.com>
2020-07-20 10:09:20 -0400

    current work, computer giving up

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,8 @@ const setupEvents = (data, num) => {
     row.setAttribute("id", `${curRow}`);
     document.querySelector('.events').appendChild(row);
   }
+  // DAILY SCHEFS FESTIVAL SETUP: 
+  // QUERY FOR db.collection('july20Events').get(~after-x-date~) and append to div festivalDayx
   /* Element will look like <div class="row;" id="row-0;"> ... 3 events ... </div> */
 
   let html = '';
@@ -47,56 +49,55 @@ const setupEvents = (data, num) => {
       time += hour + "pm EDT";
     }
     const li = `
-      <div class="col-sm-4" style="margin-bottom: 2rem;">
-        <div class="card border-0" style="max-width: 20rem;">
-            <img src="${event.thumbnail}" alt="..." style="inline-size: 100%; border-radius: 10%;">
-            <p style="font-size:20px; margin-top: 1rem;">${event.title}
-                <br><small style="font-size:13px; line-height: 125%; ">Dinner • Columbia University<br>${time}</small>
-            </p>  
+    <div class="col-sm-4" style="margin-bottom: 2rem;>
+      <div class="card border-0" style="max-width: 20rem;">
+          <a href="template.html">
+          <img src="${event.thumbnail}"; alt="..." style="inline-size: 100%; border-radius: 10%;"></a>
+          <p style="line-height: 0.9; font-size:20px; margin-top: 1.2rem; margin-bottom: 0.8rem; font-family: 'Roboto', sans-serif;">${event.title}</p> 
+          <p style="font-size:14px; font-family:'Roboto', sans-serif; font-weight:300;">Dinner • Columbia University<br>${time}</p>       
         </div>
-      </div>` //template string
+    </div>` //template string
     html += li; // fill 3-event-buffer
 
     var thisRow=(Math.floor(count/3)).toString(); 
     
     if (rowCheck%3 === 0){  // when to make a new row and empty buffer
+      console.log("why don't you work")
       document.getElementById(`row-${thisRow}`).innerHTML = html;
       html = '';
     }
-    
-    // last line, make sure to print out incomplete rows!
-    if (rowCheck%3 === 1 && rowCheck === (num-1)){ // at second-to-last event
-      console.log("hey")
-      const secondToLast = `
-      <div class="col-sm-4" style="margin-bottom: 2rem;">
+    else{
+      // last line, make sure to print out incomplete rows!
+      if (rowCheck%3 === 1 && rowCheck === (num-1)){ // at second-to-last event
+        console.log("hey")
+        const secondToLast = `
+        <div class="col-sm-4" style="margin-bottom: 2rem;>
         <div class="card border-0" style="max-width: 20rem;">
             <img src="${event.thumbnail}" alt="..." style="inline-size: 100%; border-radius: 10%;">
-            <p style="font-size:20px; margin-top: 1rem;">${event.title}
-                <br><small style="font-size:13px; line-height: 125%; ">Dinner • Columbia University<br>${time}</small>
-            </p>  
+            <p style="line-height: 0.9; font-size:20px; margin-top: 1.2rem; margin-bottom: 0.8rem; font-family: 'Roboto', sans-serif;">${event.title}</p> 
+            <p style="font-size:14px; font-family:'Roboto', sans-serif; font-weight:300;">Dinner • Columbia University<br>${time}</p> 
         </div>
-      </div>`
-      remainder += secondToLast; // fill 3-event-buffer
+        </div>`
+        remainder += secondToLast; // fill 3-event-buffer
 
-      return;
-    } 
+        return;
+      } 
 
-    if (rowCheck === num){ // at last event
-      console.log("hi")
+      if (rowCheck === num){ // at last event
+        console.log("hi")
 
-      const last = `
-      <div class="col-sm-4" style="margin-bottom: 2rem;">
+        const last = `
+        <div class="col-sm-4" style="margin-bottom: 2rem;>
         <div class="card border-0" style="max-width: 20rem;">
             <img src="${event.thumbnail}" alt="..." style="inline-size: 100%; border-radius: 10%;">
-            <p style="font-size:20px; margin-top: 1rem;">${event.title}
-                <br><small style="font-size:13px; line-height: 125%; ">Dinner • Columbia University<br>${time}</small>
-            </p>  
+            <p style="line-height: 0.9; font-size:20px; margin-top: 1.2rem; margin-bottom: 0.8rem; font-family: 'Roboto', sans-serif;">${event.title}</p> 
+            <p style="font-size:14px; font-family:'Roboto', sans-serif; font-weight:300;">Dinner • Columbia University<br>${time}</p> 
         </div>
       </div>`
-      remainder += last;
-      document.getElementById(`row-${thisRow}`).innerHTML = remainder;
-      //html = '';
-    }
-
+        remainder += last;
+        document.getElementById(`row-${thisRow}`).innerHTML = remainder;
+        //html = '';
+      }
+    }  
   });
 } 
