e2ac0e4eb21e24898eb49f9485cd3a5e2e4fe3cd
jacquelinemarchal <64337067+jacquelinemarchal@users.noreply.github.com>
2020-11-04 12:40:42 -0800

    showing further into future

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,7 @@ const renderHomeEvents = async () => {
             let archiveEvents = [];
             snap.forEach((doc) => {
                 var data = doc.data();
-                if ((data.week === 4 || data.week === 5 || data.week === 6) && (data.status === "approved")){
+                if ((data.week === 4 || data.week === 5 || data.week === 6 || data.week === 7) && (data.status === "approved")){
                     var reference = storage.refFromURL(data.thumb)
                     allEvents.push(new Promise(async res => {
                         var url = await reference.getDownloadURL();
